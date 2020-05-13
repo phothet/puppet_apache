306b0f127051d86086e5d2b7e956301c495ef7d3
pt <pt@centbackup.waythorn.local>
2020-05-13 15:59:29 -0400

    Add new class

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include puppet_apache
+class puppet_apache {
+  include apache::install
+}
